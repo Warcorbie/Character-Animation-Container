ac6891b3edf73c69bca8b39d64064c415750aace
TOGETHER\Corbie <k.allgeyer@yahoo.de>
2017-02-15 12:52:54 +0100

    #UploadProject

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+	}
+    void Flip()
+        {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
